f8ecb3bbf60a41e9d8570900a97b0a9883d8cd60
Muhammad Shoaib Mustafa <135216302+Muhammad-Shoaib-Mustafa@users.noreply.github.com>
2026-09-16 20:57:04 +0500

    Add files via upload
    url

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const http=require("http")
-const server=http.createServer((req,res)=>
-{
-    res.end("hello from Shoaib")
-})
-server.listen(8080,()=>console.log("server is running on port 8080"))
+const server=http.createServer((req,res)=>{
+    const url=req.url
+    if(url=="/"){
+        res.writeHead(200)
+res.end("welcome to homepage")
+    }else if(url=="/courses"){
+        res.writeHead(200)
+        res.end("These are my courses")
+    } else if(url=="/tutorials"){
+        res.writeHead(200)
+        res.end("These are my tutorials")
+    }
+    else{
+        res.writeHead(404)
+        res.end("NOT FOUND")
+    }
+});
+server.listen(8080,()=>{console.log("server is running on port 8080")})
